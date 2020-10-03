078a3f95edd0634d728fc5aaab5dcd83f5f0c7eb
ScottAgirs <scott@ijs.to>
2020-10-03 15:01:08 -0400

    [add] label-has-associated-control assert either
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/jsx-a11y.js
+++ src/jsx-a11y.js
@@ -1,4 +1,10 @@
 module.exports = {
   'jsx-a11y/anchor-is-valid': 0,
+  'jsx-a11y/label-has-associated-control': [
+    2,
+    {
+      assert: 'either',
+    },
+  ],
   'jsx-a11y/label-has-for': 0,
 };
