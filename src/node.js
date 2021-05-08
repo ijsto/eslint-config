f212de99d636ee524056b3198f4e46735dc90dac
ScottAgirs <scott@ijs.to>
2021-05-08 10:21:40 -0400

    feat(Rules): Init node
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ src/node.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'node/callback-return': 'error',
+  'node/exports-style': 'error',
+  'node/no-extraneous-require': 'error',
+  'node/no-unsupported-features/es-builtins': 'error',
+  'node/no-unsupported-features/node-builtins': 'error',
+  'node/prefer-promises/fs': 'error',
+};
